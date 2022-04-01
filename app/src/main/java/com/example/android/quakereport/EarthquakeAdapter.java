6510fc4dd1092f219b4eabe487f1e687bf1ec4c1
mi-kuzeka <ckatyao@yahoo.com>
2022-04-01 13:27:55 +0500

    Add circle background for magnitude

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -41,6 +43,17 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         TextView magnitudeTextView = listItemView.findViewById(R.id.magnitude_text_view);
         magnitudeTextView.setText(formatMagnitude(currentEarthquake.getMagnitude()));
 
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeTextView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude.
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle.
+        magnitudeCircle.setColor(magnitudeColor);
+
+        // Split string of place to 2 parts
         splitPlaceText(currentEarthquake);
 
         // Set distance from place of earthquake on TextView.
@@ -63,6 +76,53 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         return listItemView;
     }
 
+    /**
+     * Return the color for the magnitude circle based on the intensity of the earthquake.
+     *
+     * @param magnitude of the earthquake
+     */
+    private int getMagnitudeColor(float magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+    }
+
+    /**
+     * Split string of the place to two part: location asset and name of place.
+     */
     private void splitPlaceText(Earthquake currentEarthquake) {
         String place = currentEarthquake.getPlace();
         if (place.contains(LOCATION_SEPARATOR)) {
@@ -79,7 +139,11 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
      * from a decimal magnitude value.
      */
     private String formatMagnitude(float magnitude) {
-        DecimalFormat format = new DecimalFormat("0.0");
+        if (magnitude < 10) {
+            DecimalFormat format = new DecimalFormat("0.0");
+            return format.format(magnitude);
+        }
+        DecimalFormat format = new DecimalFormat("0");
         return format.format(magnitude);
     }
 
