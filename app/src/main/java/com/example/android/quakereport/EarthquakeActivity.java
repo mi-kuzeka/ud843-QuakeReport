e1bfb65ce3cadd252d9de60e4dfaab82443ee1fb
mi-kuzeka <ckatyao@yahoo.com>
2022-04-21 12:45:07 +0500

    Add preference - minimum magnitude

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -18,11 +18,15 @@ package com.example.android.quakereport;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
@@ -53,7 +57,8 @@ public class EarthquakeActivity extends AppCompatActivity
      */
     private EarthquakeAdapter mAdapter;
     private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=20";
+            "https://earthquake.usgs.gov/fdsnws/event/1/query";
+//            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=20";
     /**
      * Constant value for the earthquake loader ID. We can choose any integer.
      * This really only comes into play if you're using multiple loaders.
@@ -131,7 +136,7 @@ public class EarthquakeActivity extends AppCompatActivity
             hideLoadingSpinner();
             // Set text about no internet connection
             showNoInternetText();
-            // Show view for refreshing data
+            // Show refresh image
             showRefreshView();
             return;
         }
@@ -158,7 +163,22 @@ public class EarthquakeActivity extends AppCompatActivity
     public Loader<List<Earthquake>> onCreateLoader(int id, @Nullable Bundle args) {
         Log.v(LOG_TAG, "TEST: onCreateLoader");
         hideRefreshView();
-        return new EarthquakeLoader(EarthquakeActivity.this, USGS_REQUEST_URL);
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPreferences.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("eventtype", "earthquake");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+
+        return new EarthquakeLoader(EarthquakeActivity.this, uriBuilder.toString());
     }
 
     @Override
@@ -247,5 +267,22 @@ public class EarthquakeActivity extends AppCompatActivity
 
     private void hideNoInternetText() {
         mNoInternetView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
